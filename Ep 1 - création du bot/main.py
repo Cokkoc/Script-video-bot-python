ff6d7e1705402105722fd027acc916a2adbd26f8
Cokkoc <91598592+Cokkoc@users.noreply.github.com>
2021-10-10 16:48:10 +0200

    Add files via upload

---
+++ Ep 1 - création du bot/main.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = "d!", description = "Bot de test")
+
+@bot.event
+async def on_ready():
+    print("Le bot est lancé.")
+
+@bot.event
+async def on_message(message):
+    if message.content.lower() == "ping":
+        await channel.send("pong")
+bot.run("ODk2NDI3ODQyMzc4ODEzNTMw.YWG9fA.jEjDPO_ZoPdtgg7DgNECw9D6N3I")
