ba7e89089d7c4da0fe0b0ee822a726363c16f156
Cokkoc <91598592+Cokkoc@users.noreply.github.com>
2021-11-02 16:46:09 +0100

    Update main.py

--- Ep 1 - création du bot/main.py
+++ Ep 1 - création du bot/main.py
@@ -7,9 +7,4 @@ bot = commands.Bot(command_prefix = "d!", description = "Bot de test")
 async def on_ready():
     print("Le bot est lancé.")
 
-@bot.event
-async def on_message(message):
-    if message.content.lower() == "ping":
-        await channel.send("pong")
-
 bot.run("ODk2NDI3ODQyMzc4ODEzNTMw.YWG9fA.jEjDPO_ZoPdtgg7DgNECw9D6N3I")
