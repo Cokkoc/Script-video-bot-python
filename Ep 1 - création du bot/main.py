b854d06d614a30cf205796f787d43eadef1b4119
Cokkoc <91598592+Cokkoc@users.noreply.github.com>
2021-12-24 17:43:18 +0100

    Update main.py

--- Ep 1 - création du bot/main.py
+++ Ep 1 - création du bot/main.py
@@ -1,10 +1,10 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix = "d!", description = "Bot de test")
+bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
-    print("Le bot est lancé.")
+	print("Je suis en ligne.")
 
 bot.run("ODk2NDI3ODQyMzc4ODEzNTMw.YWG9fA.jEjDPO_ZoPdtgg7DgNECw9D6N3I")
